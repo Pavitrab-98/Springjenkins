4404c72653baa682f1c9a698bae29a64da4321b7
Pavitrab-98 <pavitra.bhajantri@capgemini.com>
2022-07-04 19:54:38 +0530

    first version

---
+++ src/main/java/com/cg/SpringBootJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.cg;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+
+
+    public static Logger logger =  LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+
+    @PostConstruct
+    public void intt() {
+        logger.info("Application Started...");
+    }
+
+    public static void main(String[] args) {
+
+        logger.info("Application executed...");
+        SpringApplication.run(SpringBootJenkinsApplication.class, args);
+    }
+}
